3ca41f09e3571b8987569bd0fc6eafb72735bd99
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-31 20:26:08 +0700

    Point cut, modify aspect class

--- src/main/java/ariefbelajarteknologi/aop/aspect/LogAspect.java
+++ src/main/java/ariefbelajarteknologi/aop/aspect/LogAspect.java
@@ -1,10 +1,15 @@
 package ariefbelajarteknologi.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
 
+    @Pointcut("")
+    public void helloServiceMethod(){
+
+    }
 }
