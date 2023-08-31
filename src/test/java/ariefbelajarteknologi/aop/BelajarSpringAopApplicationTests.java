104f32db3451d73c85686cc4ec0e16c57440dcc4
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-31 11:27:57 +0700

    Initial commit

---
+++ src/test/java/ariefbelajarteknologi/aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefbelajarteknologi.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
